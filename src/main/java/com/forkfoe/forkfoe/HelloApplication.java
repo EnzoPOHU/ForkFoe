665ff8337f9cf0bac8e4cef73d679871f3d7b9a1
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2025-04-08 10:27:25 +0200

    feat: Ajout de SQLite

--- src/main/java/com/forkfoe/forkfoe/HelloApplication.java
+++ src/main/java/com/forkfoe/forkfoe/HelloApplication.java
@@ -1,5 +1,8 @@
 package com.forkfoe.forkfoe;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,8 +11,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    public static void connect() {
+        String databasePath = "database.db";
+        try (var connection = DriverManager.getConnection("jdbc:sqlite:" + databasePath)) {
+           System.out.println("[SQLite] Connected to " + databasePath) ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
+        connect();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.forkfoe.forkfoe {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.forkfoe.forkfoe to javafx.fxml;
