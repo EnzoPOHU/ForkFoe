e8f807d26246602380dd3b41a9b2df303d5c4b96
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2025-04-08 15:33:25 +0200

    Simplified Database singleton usage

--- src/main/java/com/forkfoe/forkfoe/Database.java
+++ src/main/java/com/forkfoe/forkfoe/Database.java
@@ -1,85 +1,58 @@
 package com.forkfoe.forkfoe;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public final class Database {
-    private static Database instance;
-    public Connection connection;
+    static { new Database(); }
+
+    private static Connection connection;
 
     private Database() {
         try {
-            this.connection = DriverManager.getConnection("jdbc:sqlite:database.db");
-            if (connection != null) {
-                System.out.println("[SQLite] Successfully connected to database") ;
-                this.connection = connection;
-
-                var restaurantTable = "CREATE TABLE IF NOT EXISTS restaurantTable ("
-                        + "	id INTEGER PRIMARY KEY,"
-                        + "	seat INTEGER NOT NULL,"
-                        + "	number INTEGER NOT NULL"
-                        + ");";
-
-                var client = "CREATE TABLE IF NOT EXISTS client ("
-                        + "	id INTEGER PRIMARY KEY,"
-                        + "	table_number INTEGER NOT NULL" //cléf étrangère vers number /table
-                        + ");";
-
-                var staff = "CREATE TABLE IF NOT EXISTS staff ("
-                        + "	id INTEGER PRIMARY KEY,"
-                        + "	name VARCHAR NOT NULL,"
-                        + " role VARCHAR NOT NULL"
-                        + ");";
-
-                var tableOrder = "CREATE TABLE IF NOT EXISTS tableOrder ("
-                        + "	id INTEGER PRIMARY KEY, "
-                        + " bill INTEGER, "
-                        + " status VARCHAR"
-                        + ");";
-
-                var tableDish = "CREATE TABLE IF NOT EXISTS tableDish ("
-                        + "	id INTEGER PRIMARY KEY, "
-                        + " name VARCHAR, "
-                        + " description TEXT, "
-                        + " price INTEGER, "
-                        + " img BLOB"
-                        + ");";
-
-                var orderDish = "CREATE TABLE IF NOT EXISTS orderDish ("
-                        + "	id INTEGER PRIMARY KEY, "
-                        + " orderID INTEGER, "
-                        + " dishID INTEGER, "
-                        + " quantity INTEGER "
-                        + ");";
-
-                try (Statement stmt = connection.createStatement()) {
-                    // Create new tables
-                    stmt.execute(restaurantTable);
-                    stmt.execute(client);
-                    stmt.execute(staff);
-                    stmt.execute(tableOrder);
-                    stmt.execute(tableDish);
-                    stmt.execute(orderDish);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException("Failed to create tables: " + e.getMessage());
-                }
-
-
-            }
+            connection = DriverManager.getConnection("jdbc:sqlite:database.db");
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
         }
+
+        execute("CREATE TABLE IF NOT EXISTS restaurantTable ("
+                    + "	id INTEGER PRIMARY KEY,"
+                    + "	seat INTEGER NOT NULL,"
+                    + "	number INTEGER NOT NULL);");
+
+        execute("CREATE TABLE IF NOT EXISTS client ("
+                    + "	id INTEGER PRIMARY KEY,"
+                    + "	table_number INTEGER NOT NULL);");
+
+        execute( "CREATE TABLE IF NOT EXISTS staff ("
+                    + "	id INTEGER PRIMARY KEY,"
+                    + "	name VARCHAR NOT NULL,"
+                    + " role VARCHAR NOT NULL);");
+
+        execute("CREATE TABLE IF NOT EXISTS tableOrder ("
+                    + "	id INTEGER PRIMARY KEY, "
+                    + " bill INTEGER, "
+                    + " status VARCHAR);");
+
+        execute("CREATE TABLE IF NOT EXISTS tableDish ("
+                    + "	id INTEGER PRIMARY KEY, "
+                    + " name VARCHAR, "
+                    + " description TEXT, "
+                    + " price INTEGER, "
+                    + " img BLOB);");
+
+        execute("CREATE TABLE IF NOT EXISTS orderDish ("
+                    + "	id INTEGER PRIMARY KEY, "
+                    + " orderID INTEGER, "
+                    + " dishID INTEGER, "
+                    + " quantity INTEGER);");
     }
 
-    public static Database getInstance() {
-        if (instance == null) {
-            System.out.println("test");
-            instance = new Database();
+    public static void execute(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("[SQLite] Error: " + e.getMessage());
         }
-        System.out.println("test 2");
-        return instance;
     }
 }
--- src/main/java/com/forkfoe/forkfoe/HelloApplication.java
+++ src/main/java/com/forkfoe/forkfoe/HelloApplication.java
@@ -6,12 +6,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Database.getInstance();
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
