efb4650c58671398056bab80435c88b90022d10f
nicocotier2 <nicolas.tenneguin@gmail.com>
2025-04-08 12:13:19 +0200

    add db sqlite and create table in project

--- src/main/java/com/forkfoe/forkfoe/Database.java
+++ src/main/java/com/forkfoe/forkfoe/Database.java
@@ -13,6 +13,54 @@ public final class Database {
             if (connection != null) {
                 System.out.println("[SQLite] Successfully connected to database") ;
                 this.connection = connection;
+
+                var table = "CREATE TABLE IF NOT EXISTS table ("
+                        + "	id INTEGER PRIMARY KEY,"
+                        + "	seat INTEGER NOT NULL,"
+                        + "	number INTEGER NOT NULL"
+                        + ");";
+
+                var client = "CREATE TABLE IF NOT EXISTS client ("
+                        + "	id INTEGER PRIMARY KEY,"
+                        + "	table_number INTEGER NOT NULL" //cléf étrangère vers number /table
+                        + ");";
+
+                var staff = "CREATE TABLE IF NOT EXISTS staff ("
+                        + "	id INTEGER PRIMARY KEY,"
+                        + "	name VARCHAR NOT NULL,"
+                        + " role VARCHAR NOT NULL"
+                        + ");";
+
+                var tableOrder = "CREATE TABLE IF NOT EXISTS tableOrder ("
+                        + "	id INTEGER PRIMARY KEY, "
+                        + " bill INTEGER, "
+                        + " status VARCHAR"
+                        + ");";
+
+                var tableDish = "CREATE TABLE IF NOT EXISTS tableDish ("
+                        + "	id INTEGER PRIMARY KEY, "
+                        + " name VARCHAR, "
+                        + " description TEXT, "
+                        + " price INTEGER, "
+                        + " img BLOB"
+                        + ");";
+
+                var orderDish = "CREATE TABLE IF NOT EXISTS orderDish ("
+                        + "	id INTEGER PRIMARY KEY, "
+                        + " orderID INTEGER, "
+                        + " dishID INTEGER, "
+                        + " quantity INTEGER "
+                        + ");";
+
+                var stmt = connection.createStatement();
+                    // create a new table
+                    stmt.execute(table);
+                    stmt.execute(client);
+                    stmt.execute(staff);
+                    stmt.execute(tableOrder);
+                    stmt.execute(tableDish);
+                    stmt.execute(orderDish);
+
             }
         } catch (SQLException e) {
             throw new RuntimeException(e.getMessage());
