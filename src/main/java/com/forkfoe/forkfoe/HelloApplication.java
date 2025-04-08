40e5a836cc7442d927135e5a100a2cff42ca68d9
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2025-04-08 10:47:40 +0200

    Added Database singleton

--- src/main/java/com/forkfoe/forkfoe/HelloApplication.java
+++ src/main/java/com/forkfoe/forkfoe/HelloApplication.java
@@ -1,8 +1,5 @@
 package com.forkfoe.forkfoe;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,18 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    public static void connect() {
-        String databasePath = "database.db";
-        try (var connection = DriverManager.getConnection("jdbc:sqlite:" + databasePath)) {
-           System.out.println("[SQLite] Connected to " + databasePath) ;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        connect();
+        System.out.println();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
