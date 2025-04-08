614320ef015a11f9ee7e3a9391e8c4342a5dff21
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2025-04-09 00:12:45 +0200

    feat: Employee CRUD

---
+++ src/main/java/com/forkfoe/forkfoe/Employee.java
@@ -0,0 +1,13 @@
+package com.forkfoe.forkfoe;
+
+public class Employee {
+    public String name;
+    public String role;
+    public Integer workedHours;
+
+    Employee(String name, String role, Integer workedHours) {
+        this.name = name;
+        this.role = role;
+        this.workedHours = workedHours;
+    }
+}
--- src/main/java/com/forkfoe/forkfoe/HelloApplication.java
+++ src/main/java/com/forkfoe/forkfoe/HelloApplication.java
@@ -6,9 +6,43 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 public class HelloApplication extends Application {
+    private static final String []initQueries = {
+        "CREATE TABLE IF NOT EXISTS restaurantTable (" +
+                "id INTEGER PRIMARY KEY, " +
+                "seat INTEGER NOT NULL," +
+                "number INTEGER NOT NULL)",
+
+        "CREATE TABLE IF NOT EXISTS client (" +
+                "id INTEGER PRIMARY KEY, " +
+                "table_number INTEGER NOT NULL)",
+
+        "CREATE TABLE IF NOT EXISTS employee (" +
+                "id INTEGER PRIMARY KEY, " +
+                "name VARCHAR NOT NULL, " +
+                "role VARCHAR NOT NULL, " +
+                "worked_hours INTEGER NOT NULL)",
+
+        "CREATE TABLE IF NOT EXISTS tableOrder (" +
+                "id INTEGER PRIMARY KEY," +
+                "bill INTEGER," +
+                "status VARCHAR)",
+
+        "CREATE TABLE IF NOT EXISTS tableDish (" +
+                "id INTEGER PRIMARY KEY, " +
+                "name VARCHAR, " +
+                "description TEXT, " +
+                "price INTEGER, " +
+                "img BLOB)",
+
+        "CREATE TABLE IF NOT EXISTS orderDish (" +
+                "id INTEGER PRIMARY KEY, " +
+                "orderID INTEGER, dishID INTEGER, " +
+                "quantity INTEGER)",
+    };
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -19,6 +53,21 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        SQLiteWrapper.executeBatch(initQueries);
+
+        Employee john = new Employee("John", "Server", 6);
+        Employee walter = new Employee("Walter", "Cleaner", 12);
+
+        EmployeeRepository.removeEmployee(john);
+        EmployeeRepository.removeEmployee(walter);
+
+        EmployeeRepository.addEmployee(john);
+        EmployeeRepository.addEmployee(walter);
+
+        EmployeeRepository.getEmployees().stream().forEach(employee -> {
+            System.out.println("Added " + employee.name + ", " + employee.role + ", " + employee.workedHours + " worked hours");
+        });
+
         launch();
     }
 }
