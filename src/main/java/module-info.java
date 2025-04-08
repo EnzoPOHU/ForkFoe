bee4c12a79910efdcd7ce8dd2a5fef7445151d39
Enzo POHU <enzopohu91@gmail.com>
2025-04-08 15:45:21 +0200

    fix version 23

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.forkfoe.forkfoe {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.forkfoe.forkfoe to javafx.fxml;
-    exports com.forkfoe.forkfoe;
-}
